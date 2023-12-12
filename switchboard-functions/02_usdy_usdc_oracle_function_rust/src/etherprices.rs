d5874159149bbe1e052481180f7caab37e3d3cc0
jd <jd@jds-MacBook-Pro.local>
2023-12-11 20:56:39 -0400

    ToPrimitive

--- switchboard-functions/02_usdy_usdc_oracle_function_rust/src/etherprices.rs
+++ switchboard-functions/02_usdy_usdc_oracle_function_rust/src/etherprices.rs
@@ -21,29 +21,28 @@ pub struct IndexData {
     pub data: Ticker,
 }
 impl TryInto<OracleDataBorsh> for IndexData {
-    fn into(self) -> OracleDataBorsh {
+    
+    type Error = SbError;
+
+    fn try_into(self) -> Result<OracleDataBorsh, Self::Error> {
         let oracle_timestamp: i64 = std::time::SystemTime::now()
             .duration_since(std::time::UNIX_EPOCH)
-            .unwrap_or(
-                return Err(SbError::CustomMessage(
-                    "Invalid oracle_timestamp".to_string(),
-                )),
-            )
+            .map_err(|_| {
+                SbError::CustomMessage("Invalid oracle_timestamp".to_string())
+            })?
             .as_secs()
             .try_into()
-            .unwrap_or(
-                return Err(SbError::CustomMessage(
-                    "Invalid oracle_timestamp".to_string(),
-                )),
-            );
+            .map_err(|_| {
+                SbError::CustomMessage("Invalid oracle_timestamp".to_string())
+            })?;
 
-        OracleDataBorsh {
-            oracle_timestamp,
-            mean: self.data.mean.as_u64(),
-            median: self.data.median.as_u64(),
-            std: self.data.std.as_u64(),
+            switchboard_solana::Result::Ok(OracleDataBorsh {
+                oracle_timestamp,
+                mean: self.data.mean.as_u64(),
+                median: self.data.median.as_u64(),
+                std: self.data.std.as_u64(),
 
-        }
+            })
     }
 }
 
@@ -56,22 +55,15 @@ impl EtherPrices {
     // Fetch data from the EtherPrices API
     pub async fn fetch(mean:  ethers::types::U256, median:  ethers::types::U256, std:  ethers::types::U256) -> std::result::Result<EtherPrices, SbError> {
         let symbols = ["USDYUSD"];
-        let mean: I256 = mean.try_into().unwrap_or(
-            return Err(SbError::CustomMessage(
-                "Invalid mean".to_string(),
-            )),
-        );
-        let median: I256 = median.try_into().unwrap_or(
-            return Err(SbError::CustomMessage(
-                "Invalid median".to_string(),
-            )),
-        );
-        let std: I256 = std.try_into().unwrap_or(
-            return Err(SbError::CustomMessage(
-                "Invalid std".to_string(),
-            )),
-        );
-
+        let mean: I256 = mean.try_into().map_err(|_| {
+            SbError::CustomMessage("Invalid mean".to_string())
+        }).unwrap();
+        let median: I256 = median.try_into().map_err(|_| {
+            SbError::CustomMessage("Invalid median".to_string())
+        }).unwrap();
+        let std: I256 = std.try_into().map_err(|_| {
+            SbError::CustomMessage("Invalid std".to_string())
+        }).unwrap();
         Ok(EtherPrices {
             usdy_usd: {
                 let symbol = symbols[0];
@@ -94,7 +86,9 @@ impl EtherPrices {
         let rows: Vec<OracleDataWithTradingSymbol> = vec![
             OracleDataWithTradingSymbol {
                 symbol: TradingSymbol::Usdy_usdc,
-                data: self.usdy_usd.clone().into(),
+                data: self.usdy_usd.clone().try_into().map_err(|_| {
+                    SbError::CustomMessage("Invalid oracle data".to_string())
+                }).unwrap(),
             }
             // OracleDataWithTradingSymbol {
             // symbol: TradingSymbol::Sol,
